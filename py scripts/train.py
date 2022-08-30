e3d341fb6752b719dedc344c4b495dc75c27dffb
okolenchuk <okolenchuk@yandex.ru>
2022-08-30 23:53:12 +0300

    'Hym_conv_30_08_2022

---
+++ py scripts/train.py
@@ -0,0 +1,46 @@
+import os
+from tqdm.autonotebook import trange, tqdm
+
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.optim import lr_scheduler
+
+import numpy as np
+import torchvision
+from torchvision import datasets, models, transforms
+import matplotlib.pyplot as plt
+import time
+
+use_gpu = torch.cuda.is_available()
+device = torch.device("cuda")
+
+# Преобразование обучающих данных для расширения обучающей выборки и её нормализация
+# Для валидационной (тестовой) выборки только нормализация
+def data_transform (dataset_path):
+    transform_pipeline = {
+        'train': transforms.Compose([
+            transforms.RandomResizedCrop(244),
+            transforms.RandomHorizontalFlip(),
+            transforms.ToTensor(),
+            transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+        ]),
+        'val': transforms.Compose([
+            transforms.Resize(256),
+            transforms.CenterCrop(244),
+            transforms.ToTensor(),
+            transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+        ]),
+    }
+    image_datasets = {x: datasets.ImageFolder(dataset_path) for x in ['train', 'val']}
+    # специальный класс для загрузки данных в виде батчей
+    dataloaders = {x: torch.utils.data.DataLoader(image_datasets[x], batch_size=4,
+                                                  shuffle=True, num_workers=2)
+                   for x in ['train', 'val']}
+    dataset_sizes = {x: len(image_datasets[x]) for x in ['train', 'val']}
+    return dataloaders, dataset_sizes
+    # папка с данными. Если запускаете в колабе, нужно скопировать данные к себе в директорию и примонтировать диск. Если запускаете локально -- просто скачайте данные
+data_dir = './hymenoptera_data'
+
+class_names = image_datasets['train'].classes
+
