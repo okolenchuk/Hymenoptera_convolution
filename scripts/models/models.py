16ddf5ee5be0518ce9227a94b65bf1cc325bc450
okolenchuk <okolenchuk@yandex.ru>
2022-09-03 23:11:02 +0300

    Hym_conv_03_09_2022

--- scripts/models/models.py
+++ scripts/models/models.py
@@ -3,24 +3,24 @@ from Resnet18_models import *
 
 
 # ResNet18 custom
-def ResNet18_cus(n_classes):
+def resnet18_cus(n_classes):
     return Resnet18Custom(n_classes)
 
 
 # VGG16 custom
-def VGG16Custom():
+def vgg16custom():
     return
 
 
 # ResNet18 finetune
-def Resnet18(num_classes=2, pretrained=True):
+def resnet18(num_classes=2, pretrained=True):
     model = torch.hub.load('pytorch/Resnet18', 'resnet18', pretrained=pretrained)
     model.fc = nn.Linear(512, num_classes)
     return model
 
 
 # VGG16 finetune
-def VGG16(num_classes=2, pretrained=True):
+def vgg16(num_classes=2, pretrained=True):
     model = torch.hub.load('pytorch/VGG16', 'vgg16', pretrained=pretrained)
     model.classifier[6] = nn.Linear(4096, num_classes)
     return model
