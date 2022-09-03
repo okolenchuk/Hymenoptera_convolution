f4ad1bc84fa38978c4b26e7598acac5aa5620e46
okolenchuk <okolenchuk@yandex.ru>
2022-09-04 01:18:01 +0300

    Hym_conv_04_09_2022

--- run.py
+++ run.py
@@ -1,4 +1,5 @@
 import torch
+from scripts.transform import *
 import torch.nn as nn
 import torch.optim as optim
 from torch.optim import lr_scheduler
@@ -12,8 +13,6 @@ use_gpu = torch.cuda.is_available()
 device = torch.device("cuda")
 
 path = r'D:\EDUCATION\IT_academy\Test_models\Hymenoptera_convolution\hymenoptera_data\hymenoptera_data'
-
-from scripts.transform import *
 
 dataloaders = data_transform(path, batch_size=8)
 
