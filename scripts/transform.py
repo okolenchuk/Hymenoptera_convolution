aa42a9872ee49e5c491a18a3c511b26d8c0f471e
okolenchuk <okolenchuk@yandex.ru>
2022-09-03 21:38:37 +0300

    Hym_conv_03_09_2022

--- scripts/transform.py
+++ scripts/transform.py
@@ -2,33 +2,35 @@ from torchvision import datasets, transforms
 import os
 import torch
 
-def data_transform (dataset_path, batch_size=16, use_transform=False):
+
+def data_transform(dataset_path, batch_size=16, use_transform=False):
     r"""Преобразование обучающих данных для расширения обучающей выборки и её нормализация
         применяем Crop, Horizontal flip и Normalize
         Для валидационной (тестовой) выборки только нормализация
         Функция возвращает test и train dataloader """
-    transform_pipeline = {
+    normalize = transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+    transform = {
         'train': transforms.Compose([
             transforms.RandomResizedCrop(244),
             transforms.RandomHorizontalFlip(),
             transforms.ToTensor(),
-            transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+            normalize
         ]),
         'val': transforms.Compose([
             transforms.Resize(256),
             transforms.CenterCrop(244),
             transforms.ToTensor(),
-            transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+            normalize
         ]),
     }
-    if use_transform == True:
-        image_datasets = {x: datasets.ImageFolder(os.path.join(dataset_path, x), transform_pipeline[x]) for x in ['train', 'val']}
-        # специальный класс для загрузки данных в виде батчей
+
+    if use_transform:
+        image_datasets = {x: datasets.ImageFolder(os.path.join(dataset_path, x), transform[x]) for x in
+                          ['train', 'val']}
         dataloaders = {x: torch.utils.data.DataLoader(image_datasets[x], batch_size=batch_size,
                                                       shuffle=True, num_workers=2) for x in ['train', 'val']}
     else:
-        image_datasets = {x: datasets.ImageFolder(os.path.join(dataset_path, x)) for x in ['train', 'val']}
-        # специальный класс для загрузки данных в виде батчей
+        image_datasets = {x: datasets.ImageFolder(os.path.join(dataset_path, x), normalize) for x in ['train', 'val']}
         dataloaders = {x: torch.utils.data.DataLoader(image_datasets[x], batch_size=batch_size,
                                                       shuffle=True, num_workers=2) for x in ['train', 'val']}
 
